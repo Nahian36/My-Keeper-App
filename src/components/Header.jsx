e33e2bfc719f840ca865575981d152afb39c064c
Nahian36 <nahian36@iut-dhaka.edu>
2021-09-21 17:01:10 +0600

    Initial commit, but the project is completed!

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Avatar from "@material-ui/core/Avatar";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <Avatar
+          alt="Nahian Ibn Asad"
+          src="https://github.com/Nahian36/FreeCodeCamp_Project_Images-/blob/main/FB_DP%20-%20Copy.jpg?raw=true"
+        />
+        Nahian's Keeper
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
